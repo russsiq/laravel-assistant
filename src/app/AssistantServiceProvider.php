42001e12ddcbb55f011fa345ef27572bf977398e
Рустам <rusiq@list.ru>
2019-11-11 05:28:24 +1000

    Create AssistantServiceProvider.php

---
+++ src/app/AssistantServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Russsiq\Assistant;
+
+use Illuminate\Support\ServiceProvider;
+
+class AssistantServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    public function provides()
+    {
+        //
+    }
+}
