cce72fb080a5bedf27450a3cdb100961aec2aa33
Рустам <rusiq@list.ru>
2019-11-13 13:57:26 +1000

    Добавлены исключения Установщика

---
+++ src/app/Exceptions/InstallerFailed.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Russsiq\Assistant\Exceptions;
+
+class InstallerFailed extends \Exception
+{
+    /**
+     * Exception message.
+     *
+     * @var string
+     */
+    protected $message = 'Well, that is sloppy logic!';
+}
