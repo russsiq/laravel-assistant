a1c01e25e05c6d09ba1e27eb10b2a0dc40d58949
Рустам <rusiq@list.ru>
2019-11-20 21:17:29 +1000

    Удалены устаревшие комментарии из маршрутизатора

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,18 +4,8 @@ Route::prefix('assistant')
     ->namespace('Russsiq\Assistant\Http\Controllers')
     ->middleware([
         'web',
-        'debugbar.disable'
     ])
     ->group(function () {
-        // dd('assistant');
-        // // Одностраничная административная панель.
-        // Route::middleware([
-        //         'auth',
-        //         'can:global.panel',
-        //     ])
-        //     ->get('/{any?}', 'PanelController')
-        //     ->name('panel');
-
         Route::get('/install', [
             'uses' => 'SystemInstall@stepСhoice',
             'as' => 'assistant.install.step_choice',
