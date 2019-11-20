704e9d2250a8dd5b5f3f06698a7defef60b3b691
Рустам <rusiq@list.ru>
2019-11-20 10:54:32 +1000

    Определены методы загрузки и публикации ресурсов поставщика

--- src/app/AssistantServiceProvider.php
+++ src/app/AssistantServiceProvider.php
@@ -21,7 +21,14 @@ class AssistantServiceProvider extends ServiceProvider implements DeferrableProv
      */
     public function boot()
     {
-        // 
+        $sourceDir = __DIR__.'/../';
+
+        $this->loadAssistantFiles($sourceDir);
+
+        // Публикация ресурсов может быть выполнена только из консоли.
+        if ($this->app->runningInConsole()) {
+            $this->publishAssistantFiles($sourceDir);
+        }
     }
 
     /**
@@ -44,5 +51,44 @@ class AssistantServiceProvider extends ServiceProvider implements DeferrableProv
         return [
             //
         ];
+    }
+
+    /**
+     * Загрузка файлов Ассистента.
+     *
+     * @param  string $sourceDir
+     * @return void
+     */
+    protected function loadAssistantFiles(string $sourceDir)
+    {
+        $this->loadRoutesFrom($sourceDir.'routes/web.php');
+        $this->loadJsonTranslationsFrom($sourceDir.'resources/lang');
+        // $this->loadTranslationsFrom($sourceDir.'resources/lang', 'assistant');
+        $this->loadViewsFrom($sourceDir.'resources/views', 'assistant');
+    }
+
+    /**
+     * Публикация файлов Ассистента.
+     * `php artisan vendor:publish --provider="Russsiq\Assistant\AssistantServiceProvider"`
+     *
+     * @param  string $sourceDir
+     * @return void
+     */
+    protected function publishAssistantFiles(string $sourceDir)
+    {
+        // php artisan vendor:publish --provider="Russsiq\Assistant\AssistantServiceProvider" --tag=config --force
+        $this->publishes([
+            $sourceDir.'config/assistant.php' => config_path('assistant.php'),
+        ], 'config');
+
+        // php artisan vendor:publish --provider="Russsiq\Assistant\AssistantServiceProvider" --tag=lang --force
+        $this->publishes([
+            $sourceDir.'resources/lang' => resource_path('lang/vendor/assistant'),
+        ], 'lang');
+
+        // php artisan vendor:publish --provider="Russsiq\Assistant\AssistantServiceProvider" --tag=views --force
+        $this->publishes([
+            $sourceDir.'resources/views' => resource_path('views/vendor/assistant'),
+        ], 'views');
     }
 }
