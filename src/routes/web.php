3516ab64640d97245beb48313d5f77e8e3b7132b
Рустам <rusiq@list.ru>
2019-11-21 22:28:47 +1000

    Сформирован перечень маршрутов Установщика

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,19 +6,27 @@ Route::prefix('assistant')
         'web',
     ])
     ->group(function () {
-        Route::get('/install', [
-            'uses' => 'SystemInstall@stepСhoice',
-            'as' => 'assistant.install.step_choice',
-            'middleware' => [
-                //
-            ],
-        ]);
+        Route::prefix('install')
+            ->namespace('Install')
+            ->group(function () {
+                // 1. Welcome
+                Route::get('/', 'WelcomeController@index')->name('assistant.install.welcome');
+                Route::post('/', 'WelcomeController@store')->name('assistant.install.welcome.store');
 
-        Route::post('/install', [
-            'uses' => 'SystemInstall@stepСhoice',
-            'as' => 'assistant.install.step_choice',
-            'middleware' => [
-                //
-            ],
-        ]);
+                // 2. Permission
+                Route::get('/permission', 'PermissionController@index')->name('assistant.install.permission');
+                Route::post('/permission', 'PermissionController@store')->name('assistant.install.permission.store');
+
+                // 3. Database
+                Route::get('/database', 'DatabaseController@index')->name('assistant.install.database');
+                Route::post('/database', 'DatabaseController@store')->name('assistant.install.database.store');
+
+                // 4. Migrate
+                Route::get('/migrate', 'MigrateController@index')->name('assistant.install.migrate');
+                Route::post('/migrate', 'MigrateController@store')->name('assistant.install.migrate.store');
+
+                // 5. Common
+                Route::get('/common', 'CommonController@index')->name('assistant.install.common');
+                Route::post('/common', 'CommonController@store')->name('assistant.install.common.store');
+            });
     });
