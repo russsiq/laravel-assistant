0be8732dcb1e3a4d6baeebe2cebc1c5d9c0e0990
Рустам <rusiq@list.ru>
2019-11-26 15:38:31 +1000

    Удалена логика проверки и миграций из Валидатора запроса

--- src/app/Http/Requests/Install/DatabaseRequest.php
+++ src/app/Http/Requests/Install/DatabaseRequest.php
@@ -3,7 +3,6 @@
 namespace Russsiq\Assistant\Http\Requests\Install;
 
 use Russsiq\Assistant\Http\Requests\Request;
-use Russsiq\Assistant\Exceptions\InstallerFailed;
 
 class DatabaseRequest extends Request
 {
@@ -17,13 +16,8 @@ class DatabaseRequest extends Request
         $input = $this->except([
             '_token',
             '_method',
-            //'created_at',
-            //'updated_at',
-            //'deleted_at',
             'submit',
         ]);
-
-        // $input['title'] = filter_var($input['title'], FILTER_SANITIZE_STRING, FILTER_FLAG_EMPTY_STRING_NULL);
 
         return $this->replace($input)
             ->merge([
@@ -117,67 +111,5 @@ class DatabaseRequest extends Request
             'DB_PASSWORD' => __('DB_PASSWORD'),
 
         ];
-    }
-
-    /**
-    * Configure the validator instance.
-    *
-    * @param  \Illuminate\Validation\Validator  $validator
-    * @return void
-    */
-    public function withValidator($validator)
-    {
-        if ($validator->passes()) {
-            $validator->after(function ($validator) {
-                try {
-                    // get validated data
-                    $data = $this->validated();
-
-                    // Set temporary DB connection
-                    config([
-                        'database.connections.mysql' => [
-                            'driver' => 'mysql',
-                            'host' =>  $data['DB_HOST'],
-                            'database' => $data['DB_DATABASE'],
-                            'prefix' => $data['DB_PREFIX'],
-                            'username' => $data['DB_USERNAME'],
-                            'password' => $data['DB_PASSWORD'],
-                            'charset' => 'utf8',
-                            'collation' => 'utf8_unicode_ci',
-                            'strict' => true,
-                            'engine' => 'InnoDB',
-                        ],
-                    ]);
-
-                    // Check DB connection and exists table
-                    \DB::purge('mysql');
-                    \DB::reconnect('mysql');
-                    \DB::setTablePrefix($data['DB_PREFIX']);
-                    \DB::connection()->getPdo();
-
-                    if (is_null(\DB::connection('mysql')->getDatabaseName())) {
-                        throw new InstallerFailed(__('msg.not_dbconnect'));
-                    }
-
-                    // 3 Manipulation of the database
-                    \DB::beginTransaction();
-
-                    // Make migrate
-                    $exitCode = \Artisan::call('migrate', ['--force' => true]);
-                    $outputMigrate = \Artisan::output();
-                    session()->flash('flash_output_migrate', $outputMigrate);
-
-                    \DB::commit();
-                } catch (\InstallerFailed $e) {
-                    $validator->errors()->add('database', $e->getMessage());
-                } catch (\PDOException $e) {
-                    $validator->errors()->add('database', $e->getMessage());
-                } finally {
-                    \DB::rollback();
-                }
-            });
-        }
-
-        return $validator;
     }
 }
