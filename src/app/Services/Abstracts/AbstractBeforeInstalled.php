edffaabe05b502c6b2d1861f522e55cbe0e204be
Рустам <rusiq@list.ru>
2019-12-02 20:18:26 +1000

    Обязательные методы: `handle`  и `validator`

--- src/app/Services/Abstracts/AbstractBeforeInstalled.php
+++ src/app/Services/Abstracts/AbstractBeforeInstalled.php
@@ -2,9 +2,18 @@
 
 namespace Russsiq\Assistant\Services\Abstracts;
 
+use Illuminate\Contracts\Validation\Validator as ValidatorContract;
+
 use Russsiq\Assistant\Services\Contracts\BeforeInstalledContract;
 
 abstract class AbstractBeforeInstalled implements BeforeInstalledContract
 {
-    //
+    /**
+     * Получить валидатор для проверки входящих данных запроса.
+     *
+     * @param  array  $data
+     *
+     * @return ValidatorContract
+     */
+    abstract protected function validator(array $data): ValidatorContract;
 }
--- src/app/Services/Contracts/BeforeInstalledContract.php
+++ src/app/Services/Contracts/BeforeInstalledContract.php
@@ -2,7 +2,17 @@
 
 namespace Russsiq\Assistant\Services\Contracts;
 
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
 interface BeforeInstalledContract
 {
-    // 
+    /**
+     * Обработка входящего запроса.
+     *
+     * @param  Request $request
+     *
+     * @return RedirectResponse
+     */
+    public function handle(Request $request): RedirectResponse;
 }
