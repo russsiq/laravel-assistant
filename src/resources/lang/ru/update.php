75cada9bb74091d25ccd50702cd42019cdfd5a19
Рустам <rusiq@list.ru>
2020-01-22 19:01:50 +1000

    Языковые строки Мастера обновлений

--- src/resources/lang/ru/update.php
+++ src/resources/lang/ru/update.php
@@ -14,6 +14,9 @@ return [
     ],
 
     'strings' => [
+        'installed_date_time' => 'Дата установки приложения: :installed_at',
+        'currently_version' => 'Текущая версия: :currently_version',
+        'available_version' => 'Доступная версия: :available_version',
 
     ],
 
@@ -41,6 +44,10 @@ return [
 
     'messages' => [
         'errors' => [
+            'isset_options' => 'Необходимо выбрать хотя бы одну опцию.',
+        ],
+
+        'success' => [
 
         ],
 
