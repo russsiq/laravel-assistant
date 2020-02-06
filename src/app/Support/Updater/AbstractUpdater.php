20bbe0df13eb2e5160c04a7b59ce0dd4e67a8fa9
Рустам <rusiq@list.ru>
2020-02-06 21:01:06 +1000

    Добавлен абстрактный класс для Мастера обновлений
    Данный класс будет содержать общие для всех дрйверов методы. А также обязует дочерние классы к реализации методов

---
+++ src/app/Support/Updater/AbstractUpdater.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Russsiq\Assistant\Support\Updater;
+
+use Russsiq\Assistant\Contracts\UpdaterContract;
+
+abstract class AbstractUpdater implements UpdaterContract
+{
+    // 
+}
