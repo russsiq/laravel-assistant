900cafafe62ada184f1a77575a039ee4f68a4d36
Рустам <rusiq@list.ru>
2020-02-07 07:06:33 +1000

    Реализация вспомогательных методов в AbstractUpdater

--- src/app/Support/Updater/AbstractUpdater.php
+++ src/app/Support/Updater/AbstractUpdater.php
@@ -10,6 +10,10 @@ use EnvManager;
 
 use Russsiq\Assistant\Contracts\UpdaterContract;
 
+use File;
+use SplFileInfo;
+use Symfony\Component\Finder\Finder;
+
 abstract class AbstractUpdater implements UpdaterContract
 {
     /**
@@ -114,4 +118,77 @@ abstract class AbstractUpdater implements UpdaterContract
      * @return bool
      */
     abstract public function update(): bool;
+
+    /**
+     * Рекурсивное удаление директорий из директории исходника,
+     * исключаемых из процесса обновления согласно конфигурации.
+     *
+     * @param  string  $sourcePath
+     * @return void
+     */
+    protected function deleteExcludeDirectories(string $sourcePath)
+    {
+        $toRemoved = Finder::create()->in($sourcePath)->directories()
+            ->path($this->excludeDirectories())->sortByName();
+
+        collect($toRemoved)->each(function (SplFileInfo $directory) {
+            File::deleteDirectory($directory->getRealPath());
+        });
+    }
+
+    /**
+     * Рекурсивное перемещение директорий с содержимым
+     * из директории исходника в корневую директорию приложения.
+     *
+     * @param  string  $sourcePath
+     * @param  string  $destinationPath
+     * @return void
+     */
+    protected function moveSourceDirectories(string $sourcePath, string $destinationPath)
+    {
+        $directories = Finder::create()->in($sourcePath)->directories()
+            ->sortByName();
+
+        collect($directories)->each(function (SplFileInfo $directory) use ($destinationPath) {
+            $destinationPath .= DIRECTORY_SEPARATOR.$directory->getRelativePath();
+
+            File::moveDirectory(
+                $directory->getRealPath(),
+                $destinationPath.DIRECTORY_SEPARATOR.$directory->getBasename()
+            );
+        });
+    }
+
+    /**
+     * Рекурсивное перемещение корневых файлов из директории исходника
+     * в корневую директорию приложения согласно конфигурации.
+     *
+     * @param  string  $sourcePath
+     * @param  string  $destinationPath
+     * @return void
+     */
+    protected function moveSourceRootFiles(string $sourcePath, string $destinationPath)
+    {
+        $files = Finder::create()->in($sourcePath)->files()
+            ->depth(0)->ignoreDotFiles(true)
+            ->name($this->allowedFiles())->sortByName();
+
+        collect($files)->each(function (SplFileInfo $file) use ($destinationPath) {
+            File::move(
+                $file->getRealPath(),
+                $destinationPath.DIRECTORY_SEPARATOR.$file->getFilename()
+            );
+        });
+    }
+
+    /**
+     * Удаление временной директории с исходниками.
+     *
+     * @param  string  $sourcePath
+     * @return bool
+     */
+    protected function deleteSourceDirectory(string $sourcePath): bool
+    {
+        return File::deleteDirectory($sourcePath);
+    }
 }
