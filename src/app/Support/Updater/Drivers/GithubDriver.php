e41267791c097d849d7c1859295cedaeaad0cb84
Рустам <rusiq@list.ru>
2020-02-06 21:26:28 +1000

    Добавлено динамическое конфигурирование параметров GithubDriver

--- src/app/Support/Updater/Drivers/GithubDriver.php
+++ src/app/Support/Updater/Drivers/GithubDriver.php
@@ -7,14 +7,50 @@ use Russsiq\Assistant\Support\Updater\AbstractUpdater;
 class GithubDriver extends AbstractUpdater
 {
     /**
+     * Массив параметров экземпляра класса.
+     *
+     * @var array
+     */
+    private $params = [
+        'exclude_directories' => [],
+        'allowed_files' => [],
+        'destination_path' => null,
+
+    ];
+
+    /**
      * Создать новый экземпляр Мастера обновлений
      * с использованием драйвера Github.
      *
-     * @param array  $options
+     * @param array  $params
      * @return void
      */
     public function __construct(array $params = [])
     {
-        //
+        $this->configure($params);
+    }
+
+    /**
+     * Конфигурирование параметров экземпляра класса.
+     *
+     * @param  array  $params
+     * @return $this
+     */
+    public function configure(array $params = [])
+    {
+        if (isset($params['exclude_directories']) and is_array($params['exclude_directories'])) {
+            // Задать папки, игнорируемые во время процесса обновления.
+            $this->params['exclude_directories'] = $params['exclude_directories'];
+        }
+
+        if (isset($params['allowed_files']) and is_array($params['allowed_files'])) {
+            // Задать файлы, расположеные в корне приложения, которые будут обновлены.
+            $this->params['allowed_files'] = $params['allowed_files'];
+        }
+
+        // Задать корневую директорию обновляемого приложения.
+        $this->params['destination_path'] = base_path('new');
+
+        return $this;
     }
 }
