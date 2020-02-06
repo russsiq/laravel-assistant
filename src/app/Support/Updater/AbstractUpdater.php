28e3a6dc8dc76ff96fe4a144089790ac02ed2e29
Рустам <rusiq@list.ru>
2020-02-06 21:05:12 +1000

    Добавлены заглушки методов абстрактного класса для Мастера обновлений

--- src/app/Support/Updater/AbstractUpdater.php
+++ src/app/Support/Updater/AbstractUpdater.php
@@ -6,5 +6,56 @@ use Russsiq\Assistant\Contracts\UpdaterContract;
 
 abstract class AbstractUpdater implements UpdaterContract
 {
-    // 
+    /**
+     * Получить дату установки приложения.
+     *
+     * @return mixed
+     */
+    public function installedAt()
+    {
+        //
+    }
+
+    /**
+     * Получить номер фактической версии приложения.
+     *
+     * @return string
+     */
+    public function currentlyVersion(): string
+    {
+        //
+    }
+
+    /**
+     * Получить номер доступной версии приложения,
+     * опубликованного в репозитории.
+     *
+     * @return string
+     */
+    abstract public function availableVersion(): string;
+
+    /**
+     * Доступность новой версии приложения в репозитории.
+     *
+     * @return bool
+     */
+    public function isNewVersionAvailable(): bool
+    {
+        //
+    }
+
+    /**
+     * Загрузить архив новой версии приложения
+     * из репозитория с помощью НТТР-метода GЕТ.
+     *
+     * @return void
+     */
+    abstract public function fetch();
+
+    /**
+     * Запустить процесс обновления приложения до актуальной версии.
+     *
+     * @return bool
+     */
+    abstract public function update(): bool;
 }
