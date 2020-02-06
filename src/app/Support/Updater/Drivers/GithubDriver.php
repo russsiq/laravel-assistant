ce90947f823e5b6cb2c71b4bfe0e042d3f6b0a5a
Рустам <rusiq@list.ru>
2020-02-06 20:58:11 +1000

    Добавлен GithubDriver для Мастера обновлений

---
+++ src/app/Support/Updater/Drivers/GithubDriver.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Russsiq\Assistant\Support\Updater\Drivers;
+
+use Russsiq\Assistant\Support\Updater\AbstractUpdater;
+
+class GithubDriver extends AbstractUpdater
+{
+    /**
+     * Создать новый экземпляр Мастера обновлений
+     * с использованием драйвера Github.
+     *
+     * @param array  $options
+     * @return void
+     */
+    public function __construct(array $params = [])
+    {
+        //
+    }
+}
