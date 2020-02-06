823146a1a4b8fe88cf71c6b27d659a21b21e8088
Рустам <rusiq@list.ru>
2020-02-06 20:21:34 +1000

    Расширена конфигурация Мастера обновлений

--- src/config/assistant.php
+++ src/config/assistant.php
@@ -89,18 +89,63 @@ return [
         // Путь к временной папке для загрузки обновления из репозитория.
         'download_path' => env('ASSISTANT_DOWNLOAD_PATH', storage_path('tmp')),
 
+        // Указанные папки будут пропущены во время процесса обновления.
+        'exclude_directories' => [
+            'bootstrap/cache',
+            'config/settings',
+            'node_modules',
+            'public',
+            'storage',
+            'vendor', // Перед релизом CMS закомментировать.
+
+        ],
+
+        // Указанные файлы расположены в корне приложения и будут обновлены.
+        // А другие файлы (например, `webpack.mix.js`)
+        // будут исключены из списка обновляемых.
+        'allowed_files' => [
+            'artisan',
+            'composer.json',
+            'LICENSE',
+            'package.json',
+            'readme.md',
+            // 'webpack.mix.js',
+
+        ],
+
+        // Настройки файла версионирования.
+        'version_file' => [
+            // Имя файла версионирования со сведениями о релизе.
+            'filename' => 'assistant-new-version',
+
+            // Время хранения файла версионирования в секундах.
+            // По факту частота проверки наличия обновлений.
+            'store_time' => 24 * 60 * 60,
+
+        ],
+
         // Формат строки имени архива, описывающий версию приложения.
+        // Например, имя вложения: `app_name-v1.0.0.zip`.
         'version_format' => env('ASSISTANT_VERSION_FORMAT', 'app_name-v__VERSION__.zip'),
 
         // Драйвер, используемый по умолчанию.
-        // Поддерживаемые типы: "github".
+        // Поддерживаемые типы: `github`.
         'driver' => 'github',
 
         // Настройки драйверов.
-        'drivers' => [
-            'github' => [
-                'driver' => 'github',
-                'repository_url' => 'https://api.github.com/repos/<vendor>/<name>/releases/latest',
+        'github' => [
+            'driver' => 'github',
+            'endpoint' => 'https://api.github.com/repos/<vendor>/<name>/releases/latest',
+            'version_key' => 'tag_name',
+            'source_key' => 'zipball_url',
+
+            // Настройки HTTP клиента.
+            'guzzle' => [
+                // 'base_url' => 'https://api.github.com',
+                'headers' => [
+                    'Authorization' => 'Bearer '.env('ASSISTANT_GITHUB_ACCESS_TOKEN'),
+
+                ],
 
             ],
 
