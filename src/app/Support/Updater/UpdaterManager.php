7404f9cfd6e8d6d8259f5b8aeb50430061b568ee
Рустам <rusiq@list.ru>
2020-02-06 20:46:02 +1000

    Реализация метода `getDefaultDriver` менеджера Мастера обновлений

--- src/app/Support/Updater/UpdaterManager.php
+++ src/app/Support/Updater/UpdaterManager.php
@@ -7,12 +7,19 @@ use Illuminate\Support\Manager;
 class UpdaterManager extends Manager
 {
     /**
+     * Драйвер репозитория, используемый по умолчанию.
+     *
+     * @var string
+     */
+    protected $defaultRepository = 'github';
+
+    /**
      * Получить имя драйвера, используемого по умолчанию
      *
      * @return string
      */
     public function getDefaultDriver(): string
     {
-        // 
+        return $this->config->get('assistant.updater.driver', $this->defaultRepository);
     }
 }
