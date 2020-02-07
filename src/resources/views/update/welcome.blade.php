68647024d0876b2c32b5dcf47bb3863ce9432935
Рустам <rusiq@list.ru>
2020-02-08 02:22:07 +1000

    Скорректированы языковые строки и шаблоны Мастера обновлений

--- src/resources/views/update/welcome.blade.php
+++ src/resources/views/update/welcome.blade.php
@@ -17,19 +17,11 @@
 		<ul class="alert-list">
 			<li>@lang('assistant::update.strings.installed_date_time', compact('installed_at'))</li>
 			<li>@lang('assistant::update.strings.currently_version', compact('currently_version'))</li>
-			@if ($available_version)
+			@if ($is_new_version_available)
 				<li>@lang('assistant::update.strings.available_version', compact('available_version'))</li>
+			@else
+				<li>@lang('assistant::update.strings.is_actual_version')</li>
 			@endif
 		</ul>
 	</div>
-
-	<fieldset>
-		<legend>@lang('assistant::update.forms.legends.clear')</legend>
-		<div class="form-group{{ $errors->has('clear_cache') ? ' has-error' : '' }}">
-			<label><input type="checkbox" name="clear_cache" value="1" /> @lang('assistant::update.forms.attributes.clear_cache')</label>
-            @if ($errors->has('clear_cache'))
-				<div class="invalid-feedback d-block">{{ $errors->first('clear_cache') }}</div>
-			@endif
-		</div>
-	</fieldset>
 @endsection
