a913a16b00b68fd75bb6b167b73a07a002a6a76d
Рустам <rusiq@list.ru>
2020-03-07 14:56:36 +1000

    Наброски минимального набора методов для контракта Архивариуса

--- src/app/Contracts/ArchivistContract.php
+++ src/app/Contracts/ArchivistContract.php
@@ -9,20 +9,37 @@ namespace Russsiq\Assistant\Contracts;
 interface ArchivistContract
 {
     /**
-     * Создать резервную копию.
+     * Создать резервную копию в соответствии с выбранными опциями.
+     * @param  array  $options
      * @return void
      */
-    public function backup();
+    public function backup(array $options = []);
 
     /**
-     * Получить коллекцию файлов с архивами.
+     * Получить коллекцию файлов резервных копий,
+     * включая их свойства: имя, размер, дата создания.
      * @return array
      */
     public function backups();
 
     /**
-     * Восстановить резервную копию.
+     * Восстановить резервную копию в соответствии с выбранными опциями.
+     * @param  string  $filename
+     * @param  array  $options
      * @return void
      */
-    public function restore();
+    public function restore(string $filename, array $options = []);
+
+    /**
+     * Удалить файл резервной копии.
+     * @param  string  $filename
+     * @return bool
+     */
+    public function deleteBackup(string $filename);
+
+    /**
+     * Удалить все файлы резервных копий.
+     * @return bool
+     */
+    public function deleteAllBackups();
 }
