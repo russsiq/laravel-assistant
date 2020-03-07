9b5ba84aba46d08734118fb153d19a1c300e4120
Рустам <rusiq@list.ru>
2020-03-07 10:45:28 +1000

    Метод получения допустимых значений для правила `in:список_значений`

--- src/app/Http/Requests/Request.php
+++ src/app/Http/Requests/Request.php
@@ -6,6 +6,24 @@ use Illuminate\Foundation\Http\FormRequest;
 
 abstract class Request extends FormRequest
 {
+    /**
+     * Общий массив допустимых значений для правила `in:список_значений`.
+     * @var array
+     */
+    protected $allowedForInRule = [
+        // 'key' => ['one', 'two', 'other'],
+    ];
+
+    /**
+     * Получить список допустимых значений для правила `in:список_значений`.
+     * @param  string  $key
+     * @return string
+     */
+    protected function allowedForInRule(string $key): string
+    {
+        return implode(',', $this->allowedForInRule[$key]);
+    }
+
     public function authorize()
     {
         return true;
