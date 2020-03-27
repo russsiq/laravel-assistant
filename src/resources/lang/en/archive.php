75c0dbad5247ce07a626e54127c26655e55b029c
Рустам <rusiq@list.ru>
2020-03-27 12:02:47 +1000

    Синхронизация языковых файлов

--- src/resources/lang/en/archive.php
+++ src/resources/lang/en/archive.php
@@ -19,14 +19,36 @@ return [
 
     'forms' => [
         'legends' => [
+            'backup' => 'Создать резервную копию',
+            'restore' => 'Восстановление из архива',
 
         ],
 
         'attributes' => [
+            'filename' => 'Файл резервной копии',
+            'backup_database' => 'Дамп базы данных',
+            'backup_system' => 'Системные файлы',
+            'backup_theme' => 'Активная тема',
+            'backup_uploads' => 'Вложенные файлы',
+
+            'restore_database' => 'Дамп базы данных',
+            'restore_system' => 'Системные файлы',
+            'restore_theme' => 'Активная тема',
+            'restore_uploads' => 'Вложенные файлы',
 
         ],
 
         'labels' => [
+            'filename' => 'Список файлов резервных копий',
+            'backup_database' => 'Дамп базы данных',
+            'backup_system' => 'Системные файлы',
+            'backup_theme' => 'Активная тема',
+            'backup_uploads' => 'Вложенные файлы',
+
+            'restore_database' => 'Дамп базы данных',
+            'restore_system' => 'Системные файлы',
+            'restore_theme' => 'Активная тема',
+            'restore_uploads' => 'Вложенные файлы',
 
         ],
 
@@ -35,7 +57,7 @@ return [
         ],
 
         'validation' => [
-
+            'restore.required_without' => 'Необходимо выбрать хотя бы одну опцию.'
         ],
     ],
 
