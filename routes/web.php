96a78913eb786502d6568a98c97ca515a54e4db0
russsiq <rusiq@list.ru>
2021-01-25 12:24:19 +1000

    Изменит определения маршрутов в соответствии с Laravel 8.x

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Russsiq\Assistant\Http\Controllers\Archive\WelcomeController as ArchiveWelcomeController;
+use Russsiq\Assistant\Http\Controllers\Clean\WelcomeController as CleanWelcomeController;
+use Russsiq\Assistant\Http\Controllers\Install\DatabaseController as InstallDatabaseController;
+use Russsiq\Assistant\Http\Controllers\Install\CommonController as InstallCommonController;
+use Russsiq\Assistant\Http\Controllers\Install\PermissionController as InstallPermissionController;
+use Russsiq\Assistant\Http\Controllers\Install\WelcomeController as InstallWelcomeController;
+use Russsiq\Assistant\Http\Controllers\Update\WelcomeController as UpdateWelcomeController;
+
 /**
  * Описание посредников:
  *  `can:use-assistant` - текущий пользователь имеет право воспользоваться Ассистентом приложения.
@@ -7,80 +16,74 @@
  */
 
 Route::prefix('assistant')
-    ->namespace('Russsiq\Assistant\Http\Controllers')
     ->middleware([
         'web',
-    ])
-    ->group(function () {
+    ])->group(function () {
         Route::prefix('archive')
-            ->namespace('Archive')
             ->middleware([
                 'can:use-assistant',
             ])
             ->group(function () {
                 // 1. Welcome
-                Route::get('/', 'WelcomeController@index')->name('assistant.archive.welcome');
-                Route::post('/', 'WelcomeController@store')->name('assistant.archive.welcome.store');
+                Route::get('/', [ArchiveWelcomeController::class, 'index'])->name('assistant.archive.welcome');
+                Route::post('/', [ArchiveWelcomeController::class, 'store'])->name('assistant.archive.welcome.store');
 
                 // 2. Сomplete
-                Route::get('/complete', 'WelcomeController@complete')->name('assistant.archive.complete');
-                Route::post('/complete', 'WelcomeController@redirect')->name('assistant.archive.redirect');
+                Route::get('/complete', [ArchiveWelcomeController::class, 'complete'])->name('assistant.archive.complete');
+                Route::post('/complete', [ArchiveWelcomeController::class, 'redirect'])->name('assistant.archive.redirect');
             });
 
         Route::prefix('clean')
-            ->namespace('Clean')
             ->middleware([
                 'can:use-assistant',
             ])
             ->group(function () {
                 // 1. Welcome
-                Route::get('/', 'WelcomeController@index')->name('assistant.clean.welcome');
-                Route::post('/', 'WelcomeController@store')->name('assistant.clean.welcome.store');
+                Route::get('/', [CleanWelcomeController::class, 'index'])->name('assistant.clean.welcome');
+                Route::post('/', [CleanWelcomeController::class, 'store'])->name('assistant.clean.welcome.store');
 
                 // 2. Сomplete
-                Route::get('/complete', 'WelcomeController@complete')->name('assistant.clean.complete');
-                Route::post('/complete', 'WelcomeController@redirect')->name('assistant.clean.redirect');
+                Route::get('/complete', [CleanWelcomeController::class, 'complete'])->name('assistant.clean.complete');
+                Route::post('/complete', [CleanWelcomeController::class, 'redirect'])->name('assistant.clean.redirect');
             });
 
         Route::prefix('install')
-            ->namespace('Install')
             ->middleware([
                 'already-installed',
             ])
             ->group(function () {
                 // 1. Welcome
-                Route::get('/', 'WelcomeController@index')->name('assistant.install.welcome');
-                Route::post('/', 'WelcomeController@store')->name('assistant.install.welcome.store');
+                Route::get('/', [InstallWelcomeController::class, 'index'])->name('assistant.install.welcome');
+                Route::post('/', [InstallWelcomeController::class, 'store'])->name('assistant.install.welcome.store');
 
                 // 2. Permission
-                Route::get('/permission', 'PermissionController@index')->name('assistant.install.permission');
-                Route::post('/permission', 'PermissionController@store')->name('assistant.install.permission.store');
+                Route::get('/permission', [InstallPermissionController::class, 'index'])->name('assistant.install.permission');
+                Route::post('/permission', [InstallPermissionController::class, 'store'])->name('assistant.install.permission.store');
 
                 // 3. Database
-                Route::get('/database', 'DatabaseController@index')->name('assistant.install.database');
-                Route::post('/database', 'DatabaseController@store')->name('assistant.install.database.store');
+                Route::get('/database', [InstallDatabaseController::class, 'index'])->name('assistant.install.database');
+                Route::post('/database', [InstallDatabaseController::class, 'store'])->name('assistant.install.database.store');
 
                 // 4. Database complete
-                Route::get('/database-complete', 'DatabaseController@complete')->name('assistant.install.database-complete');
-                Route::post('/database-complete', 'DatabaseController@redirect')->name('assistant.install.database-redirect');
+                Route::get('/database-complete', [InstallDatabaseController::class, 'complete'])->name('assistant.install.database-complete');
+                Route::post('/database-complete', [InstallDatabaseController::class,'redirect'])->name('assistant.install.database-redirect');
 
                 // 5. Common
-                Route::get('/common', 'CommonController@index')->name('assistant.install.common');
-                Route::post('/common', 'CommonController@store')->name('assistant.install.common.store');
+                Route::get('/common', [InstallCommonController::class, 'index'])->name('assistant.install.common');
+                Route::post('/common', [InstallCommonController::class, 'store'])->name('assistant.install.common.store');
             });
 
         Route::prefix('update')
-            ->namespace('Update')
             ->middleware([
                 'can:use-assistant',
             ])
             ->group(function () {
                 // 1. Welcome
-                Route::get('/', 'WelcomeController@index')->name('assistant.update.welcome');
-                Route::post('/', 'WelcomeController@store')->name('assistant.update.welcome.store');
+                Route::get('/', [UpdateWelcomeController::class, 'index'])->name('assistant.update.welcome');
+                Route::post('/', [UpdateWelcomeController::class, 'store'])->name('assistant.update.welcome.store');
 
                 // 2. Сomplete
-                Route::get('/complete', 'WelcomeController@complete')->name('assistant.update.complete');
-                Route::post('/complete', 'WelcomeController@redirect')->name('assistant.update.redirect');
+                Route::get('/complete', [UpdateWelcomeController::class, 'complete'])->name('assistant.update.complete');
+                Route::post('/complete', [UpdateWelcomeController::class, 'redirect'])->name('assistant.update.redirect');
             });
     });
